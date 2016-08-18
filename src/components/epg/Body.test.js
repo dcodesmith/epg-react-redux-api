f8a6249b19a2a40dd302f4bef2aab8aa24b0e4fe
Dammmy Kolawole <dcodesmith@gmail.com>
2016-08-18 19:16:53 +0100

    Removed `.only`

--- src/components/epg/Body.test.js
+++ src/components/epg/Body.test.js
@@ -3,7 +3,7 @@ import expect from 'expect';
 import { shallow } from 'enzyme';
 import Body from './Body';
 
-describe.only('Body', () => {
+describe('Body', () => {
 
   describe('Given ...', () => {
 
