403168a739515b16913def89746178a976f0d36c
Dammmy Kolawole <dcodesmith@gmail.com>
2016-08-18 19:17:52 +0100

    Added more test cases

--- src/components/epg/ChannelList.test.js
+++ src/components/epg/ChannelList.test.js
@@ -16,8 +16,19 @@ describe.only('ChannelList', () => {
         component = shallow(<ChannelList {...props} />);
       });
 
-      it('should have', () => {
-        console.log(component.html());
+      it('should have one ul/parent element', () => {
+        expect(component.find('ul').length).toBe(1);
+      });
+
+      it(`should have one ${channels.length} li/child elements`, () => {
+        expect(component.find('ul').find('li').length).toBe(channels.length);
+      });
+
+      it(`should have child elements text content matching the channel property`, () => {
+        component.find('ul').find('li').forEach((node, index) => {
+          expect(node.find('.channel__name').text()).toEqual(channels[index].name);
+          expect(node.find('.channel__code').text()).toEqual(channels[index].code);
+        });
       });
 
     });
