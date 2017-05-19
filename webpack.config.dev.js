f36bac8f324fea6a3735b81c118edd66df955506
Dammy Kolawole <dammy.kolawole@gamesys.co.uk>
2017-05-19 17:10:15 +0100

    Added svg|png|jpg file loader

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -51,10 +51,12 @@ export default {
       { test: /(\.css)$/, loaders: ['style', 'css'] },
       { test: /\.less$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract('style-loader', 'css!postcss-loader!less') },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
       // { test: /\.(html|json)$/, exclude: /node_modules/, loader: 'raw'},
       { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+      { test: /\.(jpg|png|svg)$/, loader: 'file' },
       { test: /\.svg$/, exclude: /node_modules/, loader: 'file?name=[path][name].[ext]' }
     ]
   },
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -36,6 +36,7 @@ export default {
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
       { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.(jpg|png|svg)$/, loader: 'file' },
       { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
     ]
   }
