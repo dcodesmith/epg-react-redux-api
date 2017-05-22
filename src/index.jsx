0a326f9048231b9ea97044da000f7119104ed141
Afees D Kolawole <dcodesmith@gmail.com>
2017-05-22 22:55:24 +0100

    add initial offset functionality

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import { loadChannels } from './actions/channelActions';
 import { loadProgrammes } from './actions/programmeActions';
+import { navigate } from './actions/navigationActions';
 
 import App from './containers/App';
 
@@ -13,8 +14,21 @@ import './styles/base.less';
 
 const store = configureStore();
 
+function setInitialOffset() {
+  const currentHour = (new Date()).getHours();
+
+  if (currentHour >= 21) {
+    return 18;
+  } else if (currentHour <= 6) {
+    return 0;
+  } else {
+    return currentHour - 3;
+  }
+}
+
 store.dispatch(loadChannels());
 store.dispatch(loadProgrammes());
+store.dispatch(navigate(setInitialOffset()));
 
 render(
   <Provider store={store}>
