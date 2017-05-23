851cd9373bf57f1f2938eb1237f43de7b1e264a6
Dammy Kolawole <dcodesmith@gmail.com>
2017-05-23 16:36:44 +0100

    WIP: Time Indicator

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,11 +5,25 @@ import { sortBy } from 'lodash';
 
 import ProgrammeItem from './ProgrammeItem';
 
+import { pad } from '../utils';
+import { ITEM_WIDTH, TIME_FORMAT, ONE_MINUTE, TRACK_WIDTH } from '../constants';
+
 const Body = ({ programmes, transformStyle, onModalShow }) => {
+  const currentDate = new Date();
+  const currentTime = `${pad(currentDate.getHours())}:${currentDate.getMinutes()}`;
+
+  const hrs = currentDate.getHours();
+  const minutes = (currentDate.getMinutes() / ONE_MINUTE);
+  console.log(hrs, minutes);
+  const time = hrs + minutes; // parseFloat(`${hrs}.${minutes}`);
+
   const channels = Object.keys(programmes);
   const bodyClasses = classNames('programme-guide__body', {
     'epg-fade': channels.length > 0
   });
+
+  console.log('time', time * TRACK_WIDTH, transformStyle);
+  // console.log(currentDate.getMinutes(), ONE_MINUTE, ITEM_WIDTH);
 
   const getProgrammesNodes = (channel) => {
     const sortedProgramme = sortBy(programmes[channel], 'startTime');
@@ -21,6 +35,7 @@ const Body = ({ programmes, transformStyle, onModalShow }) => {
 
   const channelsNodes = channels.map(channel =>
     <div key={ channel } className={ `${channel} programme-guide__row` }>
+      <div className="indicator" style={ transformStyle }>Afees Adedamola Kolawole</div>
       <div className="programme-guide__row__header">
         { channel }
       </div>
@@ -42,7 +57,8 @@ const Body = ({ programmes, transformStyle, onModalShow }) => {
 
 Body.propTypes = {
   programmes: PropTypes.object.isRequired,
-  transformStyle: PropTypes.object.isRequired
+  transformStyle: PropTypes.object.isRequired,
+  onModalShow: PropTypes.func.isRequired
 };
 
 export default Body;
