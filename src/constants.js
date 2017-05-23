72f4dfe50b20aa89d6b0477e2edf59945b6a1361
Dammy Kolawole <dcodesmith@gmail.com>
2017-05-23 16:45:10 +0100

    Added ONE_MINUTE constant

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
-export const ONE_MILLISECOND = 60 * 1000;
+export const ONE_MINUTE = 60;
+export const ONE_MILLISECOND = ONE_MINUTE * 1000;
 export const TRACK_WIDTH = 75;
 export const ITEM_WIDTH = TRACK_WIDTH * 2;
 export const TRACK_LENGTH = 12;
