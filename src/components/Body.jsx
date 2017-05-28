de4c476c1d4687b9af69811fb163b27ec02a5cab
Afees D Kolawole <dcodesmith@gmail.com>
2017-05-28 18:06:57 +0100

    Removed redundant code

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,27 +3,14 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { sortBy } from 'lodash';
 
+import ChannelLogo from './ChannelLogo';
 import ProgrammeItem from './ProgrammeItem';
 
-import { pad } from '../utils';
-import { ITEM_WIDTH, TIME_FORMAT, ONE_MINUTE, TRACK_WIDTH } from '../constants';
-
 const Body = ({ programmes, transformStyle, onModalShow }) => {
-  const currentDate = new Date();
-  const currentTime = `${pad(currentDate.getHours())}:${currentDate.getMinutes()}`;
-
-  const hrs = currentDate.getHours();
-  const minutes = (currentDate.getMinutes() / ONE_MINUTE);
-  console.log(hrs, minutes);
-  const time = hrs + minutes; // parseFloat(`${hrs}.${minutes}`);
-
   const channels = Object.keys(programmes);
   const bodyClasses = classNames('programme-guide__body', {
     'epg-fade': channels.length > 0
   });
-
-  console.log('time', time * TRACK_WIDTH, transformStyle);
-  // console.log(currentDate.getMinutes(), ONE_MINUTE, ITEM_WIDTH);
 
   const getProgrammesNodes = (channel) => {
     const sortedProgramme = sortBy(programmes[channel], 'startTime');
@@ -35,9 +22,11 @@ const Body = ({ programmes, transformStyle, onModalShow }) => {
 
   const channelsNodes = channels.map(channel =>
     <div key={ channel } className={ `${channel} programme-guide__row` }>
+
       <div className="indicator" style={ transformStyle }>Afees Adedamola Kolawole</div>
+
       <div className="programme-guide__row__header">
-        { channel }
+        <ChannelLogo channel={ channel } />
       </div>
 
       <div className="programme-guide__row__items" style={ transformStyle }>
@@ -45,6 +34,7 @@ const Body = ({ programmes, transformStyle, onModalShow }) => {
           { getProgrammesNodes(channel) }
         </ul>
       </div>
+
     </div>
   );
 
