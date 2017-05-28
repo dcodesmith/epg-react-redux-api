002413799bd59875772832d7178d248d5091f256
Afees D Kolawole <dcodesmith@gmail.com>
2017-05-28 10:24:58 +0100

    Go to beginning of day on Date Select

--- src/actions/dateSelectorActions.js
+++ src/actions/dateSelectorActions.js
@@ -1,10 +1,11 @@
-import { SELECT_DATE } from './actionTypes';
+import { SELECT_DATE, NAVIGATE_PAGE } from './actionTypes';
+import { navigatePage } from './navigationActions';
 
-const goToNextDate = index => ({
-  type: SELECT_DATE,
-  index
-});
+export const goToNextDate = index => ({ type: SELECT_DATE, index });
 
-export const move = index => (dispatch) => {
+export const move = index => (dispatch, getState) => {
+  const offset = -Math.abs(getState().offset);
+
   dispatch(goToNextDate(index));
+  dispatch(navigatePage(offset));
 };
