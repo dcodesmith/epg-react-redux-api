adb8df4c48acc0df3069c2a09017d2b0aaa57bf9
Dammy Kolawole <dcodesmith@gmail.com>
2017-06-02 09:34:12 +0100

    Refactored

--- src/components/ChannelList.test.jsx
+++ src/components/ChannelList.test.jsx
@@ -1,32 +1,41 @@
 import React from 'react';
-import expect from 'expect';
+import { expect } from 'chai';
 import { shallow } from 'enzyme';
+
 import ChannelList from './ChannelList';
+
+const DEFAULT_PROPS = {
+  channels: [{ id: 1, name: 'name', code: '0lo08w' }]
+};
+
+const render = (testProps = {}) => {
+  const props = Object.assign({}, DEFAULT_PROPS, testProps);
+
+  return shallow(<ChannelList {...props} />);
+};
 
 describe('ChannelList', () => {
   describe('Given a channel', () => {
-    let channels = [{ id: 1, name: 'name', code: '0lo08w' }];
-
     describe('When the the component is rendered', () => {
-      let component;
+      let component, channelList;
 
-      beforeEach(() => {
-        let props = { channels };
-        component = shallow(<ChannelList {...props} />);
+      before(() => {
+        component = render();
+        channelList = component.find('.channel__list');
       });
 
       it('should have 1 ul/parent element', () => {
-        expect(component.find('ul').length).toBe(1);
+        expect(channelList.length).to.equal(1);
       });
 
-      it(`should have ${channels.length} li/child elements`, () => {
-        expect(component.find('ul').find('li').length).toBe(channels.length);
+      it(`should have ${DEFAULT_PROPS.channels.length} li/child elements`, () => {
+        expect(channelList.find('li').length).to.equal(DEFAULT_PROPS.channels.length);
       });
 
       it('should have child elements text content matching the channel property', () => {
-        component.find('ul').find('li').forEach((node, index) => {
-          expect(node.find('.channel__name').text()).toEqual(channels[index].name);
-          expect(node.find('.channel__code').text()).toEqual(channels[index].code);
+        channelList.find('li').forEach((node, index) => {
+          expect(node.find('.channel__name').text()).to.equal(DEFAULT_PROPS.channels[index].name);
+          expect(node.find('.channel__code').text()).to.equal(DEFAULT_PROPS.channels[index].code);
         });
       });
     });
