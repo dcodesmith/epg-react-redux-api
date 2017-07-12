dc948605a25546cee2735cf05d19a7411d915915
Afees D Kolawole <dcodesmith@gmail.com>
2017-07-12 21:56:28 +0100

    Added overlay test

---
+++ src/components/Overlay.test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import sinon from 'sinon';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import Modal from 'react-modal';
+
+import Overlay from './Overlay';
+import ProgrammeDetails from './ProgrammeDetails';
+
+const onHideModalSpy = sinon.spy();
+const modalProps = {
+  isOpen: false, style: {}, contentLabel: 'A modal'
+};
+
+const DEFAULT_PROPS = { isOpen: false, onHideModal: onHideModalSpy, data: {} };
+
+const render = (testProps = {}) => {
+  const props = Object.assign({}, DEFAULT_PROPS, testProps);
+
+  return shallow(<Overlay { ...props } />);
+};
+
+describe('Overlay', () => {
+  describe('Given a Overlay component', () => {
+    describe('When rendered', () => {
+      let component, button, modalComponent, programmeDetailsComponent;
+
+      beforeEach(() => {
+        component = render();
+        modalComponent = component.find(Modal);
+        programmeDetailsComponent = modalComponent.find(ProgrammeDetails);
+        button = modalComponent.find('button');
+      });
+
+      it('should render a Modal component with the appropriate props', () => {
+        expect(modalComponent.prop('isOpen')).to.be.false;
+      });
+
+      it('should render ProgrammeDetails with the approprate props as a child of the Modal Component', () => {
+        expect(programmeDetailsComponent.prop('data')).to.eql(DEFAULT_PROPS.data);
+      });
+
+      describe('And the close CTA is clicked', () => {
+        before(() => {
+          button.simulate('click');
+        });
+
+        it('should invoke the onHideModal method', () => {
+          expect(onHideModalSpy).to.be.calledOnce;
+        });
+      });
+    });
+  });
+});
