b938ebc93b369f714417d15c0d0ff81e4614dc37
Afees Kolawole <Afees D Kolawole>
2018-01-07 19:46:48 +0000

    Remove Indicator component

--- src/components/ProgrammeDetails.jsx
+++ src/components/ProgrammeDetails.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// import Grid from './Grid';
-// import LogoImg from '../images/art.jpg';
-
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
